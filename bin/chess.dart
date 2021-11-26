cae85d30b265442dc5015c541cd5fb1d7a938d76
levmorgot <levmorgot@gmail.com>
2021-11-26 14:54:18 +0300

    Добавление класса game и возможность двигать фигуры по доске

--- bin/chess.dart
+++ bin/chess.dart
@@ -1,38 +1,11 @@
-import 'figure_factories.dart';
 import 'constants.dart';
-import 'figures.dart';
-import 'player.dart';
+import 'game.dart';
 
 void main() {
-  Player player1 = Player(1, ChessFigureFactory(Color.black, Side.bottom));
-  // for (var figure in player1.figures) {
-  //   print('${figure.runtimeType.toString()[0]}, ${figure.currentPosition}');
-  // }
-  // print("_______________________________");
-  Player player2 = Player(2, ChessFigureFactory(Color.white, Side.top));
-  // for (var figure in player2.figures) {
-  //   print('${figure.runtimeType.toString()[0]}, ${figure.currentPosition}');
-  // }
-
-  final Map<SpaceName, Figure?> gameBoard = {};
-
-  chessboard.forEach((k, v) {
-    gameBoard[k] = player1.getFigureByPosition(k);
-    if (gameBoard[k] == null) {
-      gameBoard[k] = player2.getFigureByPosition(k);
-    }
-  });
-
-  for (int x = 0; x < chessboardSizeX; x++) {
-    List<String> lineFigures = [];
-    for (int y = 0; y < chessboardSizeX; y++) {
-      var type = gameBoard[SpaceName.values[y * 8 + x]].runtimeType;
-      if (type != Null) {
-        lineFigures.add(type.toString()[0]);
-      } else {
-        lineFigures.add('_');
-      }
-    }
-    print(lineFigures.join(' '));
-  }
+  Game game = Game(1,2);
+  game.printBoard();
+  game.move(SpaceName.a2, SpaceName.a3);
+  game.printBoard();
+  game.move(SpaceName.a5, SpaceName.a3);
+  game.printBoard();
 }
---
+++ bin/game.dart
@@ -0,0 +1,72 @@
+import 'constants.dart';
+import 'figure_factories.dart';
+import 'figures.dart';
+import 'player.dart';
+
+class Game {
+  Map<SpaceName, Figure?> gameBoard = {};
+  late Player player1;
+  late Player player2;
+
+  Game(player1Id, player2Id) {
+    player1 = Player(player1Id, ChessFigureFactory(Color.black, Side.bottom));
+    player2 = Player(player1Id, ChessFigureFactory(Color.white, Side.top));
+
+    chessboard.forEach((k, v) {
+      gameBoard[k] = player1.getFigureByPosition(k);
+      if (gameBoard[k] == null) {
+        gameBoard[k] = player2.getFigureByPosition(k);
+      }
+    });
+  }
+
+  void printBoard() {
+    print([
+      '   ',
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+    ].join(' '));
+    print('___________________');
+    for (int x = chessboardSizeX - 1; x >= 0; x--) {
+      List<String> lineFigures = [];
+      for (int y = 0; y < chessboardSizeX; y++) {
+        var type = gameBoard[SpaceName.values[y * 8 + x]].runtimeType;
+        if (type != Null) {
+          lineFigures.add(type.toString()[0]);
+        } else {
+          lineFigures.add('-');
+        }
+      }
+      lineFigures.insert(0, '${x + 1} |');
+      print(lineFigures.join(' '));
+    }
+    print('___________________');
+    print([
+      '   ',
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+    ].join(' '));
+  }
+
+  void move(SpaceName startPoint, SpaceName endPoint) {
+    try {
+      Figure figure = gameBoard[startPoint]!;
+      gameBoard[startPoint] = null;
+      gameBoard[endPoint] = figure;
+    } catch (_) {
+      throw Exception('Начальная клетка пуста');
+    }
+  }
+}
